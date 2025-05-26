52e1885503794d7ee529acd7a74d647d3de7032c
Ladoxer <ladderdoxer@gmail.com>
2025-05-26 17:46:05 +0530

    fix: message mdoule providers added

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { MessageService } from './message.service';
 import { MessageController } from './message.controller';
 import { RedisModule } from '../redis/redis.module';
+import { UserService } from 'src/user/user.service';
+import { AnalyticsService } from 'src/analytics/analytics.service';
 
 @Module({
   imports: [RedisModule],
   controllers: [MessageController],
-  providers: [MessageService],
+  providers: [MessageService, UserService, AnalyticsService],
   exports: [MessageService],
 })
 export class MessageModule {}
