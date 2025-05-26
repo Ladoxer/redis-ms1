9e0a28745ab69614587add8e6297c36ae9b83c3b
Ladoxer <ladderdoxer@gmail.com>
2025-05-26 12:14:27 +0530

    feat: App module update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RedisModule } from './redis/redis.module';
 import { MessageModule } from './message/message.module';
+import { UserModule } from './user/user.module';
+import { AnalyticsModule } from './analytics/analytics.module';
 
 @Module({
-  imports: [RedisModule, MessageModule],
+  imports: [RedisModule, MessageModule, UserModule, AnalyticsModule],
   controllers: [AppController],
   providers: [AppService],
 })
