f5d86dc604b1e3c574db221694f46e22dbfbedca
Ladoxer <ladderdoxer@gmail.com>
2025-05-27 16:32:02 +0530

    feat: notification service added to message module

--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -4,11 +4,12 @@ import { MessageController } from './message.controller';
 import { RedisModule } from '../redis/redis.module';
 import { UserService } from 'src/user/user.service';
 import { AnalyticsService } from 'src/analytics/analytics.service';
+import { NotificationService } from 'src/notification/notification.service';
 
 @Module({
   imports: [RedisModule],
   controllers: [MessageController],
-  providers: [MessageService, UserService, AnalyticsService],
+  providers: [MessageService, UserService, AnalyticsService, NotificationService],
   exports: [MessageService],
 })
 export class MessageModule {}
