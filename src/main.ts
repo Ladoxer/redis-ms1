f80a7467ccfe6ff622982a4680d6c00f98511b02
Ladoxer <ladderdoxer@gmail.com>
2025-05-30 13:41:26 +0530

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
       'http://localhost:3000',
       'http://localhost:3001',
       'http://localhost:4200',
+      'https://lado-queue-ui.vercel.app',
       'http://127.0.0.1:3000',
       'http://127.0.0.1:5500', // For Live Server (VS Code extension)
     ],
